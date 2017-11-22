9995814bb9a0504aaae068a9b071b170c878039a
monjury <monjury@gmail.com>
2017-11-21 23:16:27 -0800

    Recursion

---
+++ 30DaysOfCode/src/Recursion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/30-recursion/problem
+
+public class Recursion {
+
+	static int factorial(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int result = factorial(n);
+		System.out.println(result);
+		in.close();
+	}
+}
