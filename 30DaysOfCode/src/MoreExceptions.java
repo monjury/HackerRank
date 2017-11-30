ffcf05219b1235e9f355f29647cd87ce4a6d20e8
monjury <monjury@gmail.com>
2017-11-29 19:41:50 -0800

    MoreExceptions

---
+++ 30DaysOfCode/src/MoreExceptions.java
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/30-more-exceptions/problem
+
+public class MoreExceptions {
+
+	class Calculator {
+
+		public int power(int n, int p) {
+			if (n < 0 || p < 0) {
+				throw new IllegalArgumentException("n and p should be non-negative");
+			}
+			return (int) Math.pow(n, p);
+		}
+	}
+}
