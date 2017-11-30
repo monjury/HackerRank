19e4f42ff55c22b8f45ed4d6b3b37cb61af11ca8
monjury <monjury@gmail.com>
2017-11-29 20:00:28 -0800

    Calculator

--- 30DaysOfCode/src/MoreExceptions.java
+++ 30DaysOfCode/src/MoreExceptions.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+
 // https://www.hackerrank.com/challenges/30-more-exceptions/problem
+
+class Calculator {
+
+	public int power(int n, int p) {
+		if (n < 0 || p < 0) {
+			throw new IllegalArgumentException("n and p should be non-negative");
+		}
+		return (int) Math.pow(n, p);
+	}
+}
 
 public class MoreExceptions {
 
-	class Calculator {
+	public static void main(String[] args) {
 
-		public int power(int n, int p) {
-			if (n < 0 || p < 0) {
-				throw new IllegalArgumentException("n and p should be non-negative");
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+
+			int n = in.nextInt();
+			int p = in.nextInt();
+			Calculator myCalculator = new Calculator();
+			try {
+				int ans = myCalculator.power(n, p);
+				System.out.println(ans);
 			}
-			return (int) Math.pow(n, p);
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 		}
+		in.close();
 	}
 }
